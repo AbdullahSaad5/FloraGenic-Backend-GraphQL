cf3ed99ed78f6e0dbb0f9e4844a318126c0d6781
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-25 21:56:46 +0500

    Removed console logs from union resolver

--- Schema/UnionResolvers.js
+++ Schema/UnionResolvers.js
@@ -2,17 +2,14 @@ export const UnionResolvers = {
   UserDetails: {
     __resolveType: (obj, args, info) => {
       if (obj.services && obj.CNIC) {
-        console.log("Gardener");
         return "Gardener";
       }
       if (!obj.services && obj.CNIC) {
-        console.log("Admin");
         return "Admin";
       }
       if (obj.nurseries) {
         return "NurseryOwner";
       }
-      console.log("Customer");
       return "Customer";
     },
   },
