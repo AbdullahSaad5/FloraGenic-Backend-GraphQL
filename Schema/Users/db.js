0c6b17723701facc30dc008814d5df3ed97dde6f
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-25 13:14:29 +0500

    Added password hashing on update password

--- Schema/Users/db.js
+++ Schema/Users/db.js
@@ -53,6 +53,26 @@ userSchema.pre("save", function (next) {
   });
 });
 
+userSchema.pre("findOneAndUpdate", function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 // TODO Implement password reset method
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
