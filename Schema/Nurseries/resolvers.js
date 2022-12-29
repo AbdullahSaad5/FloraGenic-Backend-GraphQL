9f625b2c3a89f39590e70e33b70fc36418e9068d
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-29 15:28:12 +0500

    Fixed nursery owner id bug

--- Schema/Nurseries/resolvers.js
+++ Schema/Nurseries/resolvers.js
@@ -1,1 +1,12 @@
-export const NurseryResolvers = {};
+import { NurseryOwnerModel } from "../NurseryOwner/db.js";
+export const NurseryResolvers = {
+  nurseryOwnerID: async (parent) => {
+    const nurseryOwner = await NurseryOwnerModel.findOne({
+      nurseries: {
+        $in: [parent._id],
+      },
+    });
+    if (!nurseryOwner) throw new Error("Nursery Owner not found");
+    return nurseryOwner._id;
+  },
+};
