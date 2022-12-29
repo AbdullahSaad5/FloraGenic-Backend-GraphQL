2cac46d61516cc82e95951f374d35253bea7f56a
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2022-12-29 15:33:39 +0500

    added category details resolver

--- Schema/Products/resolvers.js
+++ Schema/Products/resolvers.js
@@ -1,7 +1,12 @@
 import { NurseryModel } from "../Nurseries/db.js";
+import { CategoryModel } from "../Category/db.js";
 export const ProductResolvers = {
   nursery: async (parent) => {
     const nursery = await NurseryModel.findById(parent.nurseryID);
     return nursery;
+  },
+  category: async (parent) => {
+    const category = await CategoryModel.findById(parent.category);
+    return category;
   },
 };
