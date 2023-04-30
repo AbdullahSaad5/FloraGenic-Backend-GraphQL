52f5ac4bf42540039d4a2102feb076ec9438d865
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-04-30 20:34:44 +0500

    Added JWT auth to products

--- Schema/Products/query.js
+++ Schema/Products/query.js
@@ -1,9 +1,11 @@
 import { NurseryModel } from "../Nurseries/db.js";
+import { NurseryOwnerModel } from "../NurseryOwner/db.js";
 import { ProductModel } from "./db.js";
 export const ProductQuery = {
   products: async (parent, args, context) => {
     const query = {};
-    const userType = context.userType || "Customer";
+    const userType = context?.user?.userType || "Customer";
+
     if (args.data) {
       const { name, description, category } = args.data;
       if (name) {
@@ -17,15 +19,42 @@ export const ProductQuery = {
         query.category = { $regex: category, $options: "i" };
       }
     }
+
+    let products = [];
+
     if (userType === "NurseryOwner") {
-      const nursery = await NurseryModel.findOne({ owner: context.user._id });
+      const nurseyOwner = await NurseryOwnerModel.findOne({
+        userID: context.user._id,
+      });
+
+      products = await ProductModel.find({
+        nurseryID: {
+          $in: nurseyOwner.nurseries,
+        },
+      }).sort({ name: 1 });
+    } else {
+      products = await ProductModel.find({ ...query }).sort({ name: 1 });
     }
-    const products = await ProductModel.find({ ...query }).sort({ name: 1 });
+
     return products;
   },
   product: async (parent, args) => {
     const { id } = args;
-    const product = await ProductModel.findById(id);
+    let product;
+    if (userType === "NurseryOwner") {
+      const nurseyOwner = await NurseryOwnerModel.findOne({
+        userID: context.user._id,
+      });
+
+      product = await ProductModel.findOne({
+        nurseryID: {
+          $in: nurseyOwner.nurseries,
+        },
+        _id: id,
+      });
+    } else {
+      product = await ProductModel.findById(id);
+    }
     return product;
   },
 };
