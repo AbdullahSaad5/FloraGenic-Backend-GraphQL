f06b5f6dd46ffa54df1764bc9fab7a93ebff10bc
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-05-13 11:56:39 +0500

    Added nursery check in nurseries

--- Schema/Nurseries/query.js
+++ Schema/Nurseries/query.js
@@ -1,19 +1,55 @@
 import { NurseryModel } from "./db.js";
 import { NurseryOwnerModel } from "../NurseryOwner/db.js";
 export const NurseryQuery = {
-  nurseries: async () => {
-    const nurseries = await NurseryModel.find();
+  nurseries: async (parent, args, context) => {
+    const { user } = context;
+    let nurseries;
+    if (user.userType === "NurseryOwner") {
+      const nurseryOwner = await NurseryOwnerModel.findOne({
+        userId: user._id,
+      });
+      nurseries = await NurseryModel.find({
+        _id: { $in: nurseryOwner.nurseries },
+      });
+    } else {
+      nurseries = await NurseryModel.find();
+    }
     return nurseries;
   },
-  nursery: async (parent, args) => {
-    const nursery = await NurseryModel.findById(args.id);
+  nursery: async (parent, args, context) => {
+    const { user } = context;
+    let nursery;
+    if (user.userType === "NurseryOwner") {
+      const nurseryOwner = await NurseryOwnerModel.findOne({
+        userId: user._id,
+      });
+      nursery = await NurseryModel.findOne({
+        _id: args.id,
+        _id: { $in: nurseryOwner.nurseries },
+      });
+    } else {
+      nursery = await NurseryModel.findById(args.id);
+    }
+
     return nursery;
   },
-  nurserySearch: async (parent, args) => {
+  nurserySearch: async (parent, args, context) => {
     const { search } = args;
-    const nurseries = await NurseryModel.find({
-      name: { $regex: search, $options: "i" },
-    });
+    const { user } = context;
+    let nurseries;
+    if (user.userType === "NurseryOwner") {
+      const nurseryOwner = await NurseryOwnerModel.findOne({
+        userId: user._id,
+      });
+      nurseries = await NurseryModel.find({
+        name: { $regex: search, $options: "i" },
+        _id: { $in: nurseryOwner.nurseries },
+      });
+    } else {
+      nurseries = await NurseryModel.find({
+        name: { $regex: search, $options: "i" },
+      });
+    }
     return nurseries;
   },
 };
