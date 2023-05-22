cd89650bef50398ebb128c3b4dbda7552672c6a1
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-05-22 14:55:09 +0500

    Pre functions in reviews

--- Schema/Reviews/db.js
+++ Schema/Reviews/db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+import { ProductModel } from "../Products/db";
+import { GardenerModel } from "../Gardeners/db";
+import { NurseryModel } from "../Nurseries/db";
+
 const reviewSchema = new Schema({
   userID: {
     type: String,
@@ -41,6 +45,60 @@ const reviewSchema = new Schema({
 
 reviewSchema.pre("findOneAndUpdate", function (next) {
   this.update({}, { $set: { updatedAt: new Date() } });
+  next();
+});
+
+reviewSchema.pre("save", function (next) {
+  const review = this;
+  if (review.productType === "Product") {
+    ProductModel.findById(review.productID).then(async (product) => {
+      const allReviews = await ReviewModel.find({ productID: product._id });
+      product.overallRating =
+        product.overallRating * (allReviews.length - 1) + review.rating;
+      product.overallRating /= allReviews.length;
+    });
+  } else if (review.productType === "Gardener") {
+    GardenerModel.findById(review.productID).then(async (gardener) => {
+      const allReviews = await ReviewModel.find({ productID: gardener._id });
+      gardener.overallRating =
+        gardener.overallRating * (allReviews.length - 1) + review.rating;
+      gardener.overallRating /= allReviews.length;
+    });
+  } else if (review.productType === "Nursery") {
+    NurseryModel.findById(review.productID).then(async (nursery) => {
+      const allReviews = await ReviewModel.find({ productID: nursery._id });
+      nursery.overallRating =
+        nursery.overallRating * (allReviews.length - 1) + review.rating;
+      nursery.overallRating /= allReviews.length;
+    });
+  }
+  next();
+});
+
+reviewSchema.pre("deleteOne", function (next) {
+  const review = this;
+  if (review.productType === "Product") {
+    ProductModel.findById(review.productID).then(async (product) => {
+      const allReviews = await ReviewModel.find({ productID: product._id });
+      product.overallRating =
+        product.overallRating * allReviews.length - review.rating;
+      product.overallRating /= allReviews.length - 1;
+    });
+  } else if (review.productType === "Gardener") {
+    GardenerModel.findById(review.productID).then(async (gardener) => {
+      const allReviews = await ReviewModel.find({ productID: gardener._id });
+      product.overallRating =
+        product.overallRating * allReviews.length - review.rating;
+      product.overallRating /= allReviews.length - 1;
+    });
+  } else if (review.productType === "Nursery") {
+    NurseryModel.findById(review.productID).then(async (nursery) => {
+      const allReviews = await ReviewModel.find({ productID: nursery._id });
+      product.overallRating =
+        product.overallRating * allReviews.length - review.rating;
+      product.overallRating /= allReviews.length - 1;
+    });
+  }
   next();
 });
 
