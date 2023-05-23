6d6bdda97a671dbbcc975ad744a60220c70c4f48
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-05-23 10:24:57 +0500

    Added Gardener Order Resolvers

--- Schema/GardenerOrder/resolvers.js
+++ Schema/GardenerOrder/resolvers.js
@@ -4,16 +4,13 @@ import { GardenerModel } from "../Gardeners/db.js";
 export const GardenerOrderResolvers = {
   customer: async (parent, args, ctx) => {
     const { customer } = parent;
-    const customerData = await CustomerModel.findOne({
-      userID: customer,
-    });
+    const customerData = await CustomerModel.findById(customer);
+    console.log(customerData);
     return customerData;
   },
   gardener: async (parent, args, ctx) => {
     const { gardener } = parent;
-    const gardenerData = await GardenerModel.findOne({
-      userID: gardener,
-    });
+    const gardenerData = await GardenerModel.findById(gardener);
     return gardenerData;
   },
 };
