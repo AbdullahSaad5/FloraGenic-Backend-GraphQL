82fe54fda243df5234c7af78e5e962272ca5a6bb
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-05-23 18:45:21 +0500

    Changed address mutatuons

--- Schema/Addresses/mutation.js
+++ Schema/Addresses/mutation.js
@@ -22,7 +22,7 @@ export const AddressMutation = {
       input,
       { new: true }
     );
-    return AddressModel.find({ userID: user.id });
+    return await AddressModel.find({ userID: user.id });
   },
   addressDelete: async (root, args, context) => {
     const { id } = args;
@@ -32,7 +32,7 @@ export const AddressMutation = {
       _id: id,
       userID: user.id,
     });
-    return AddressModel.find({ userID: user.id });
+    return await AddressModel.find({ userID: user.id });
   },
   setDefaultAddress: async (root, args, context) => {
     const { id } = args;
