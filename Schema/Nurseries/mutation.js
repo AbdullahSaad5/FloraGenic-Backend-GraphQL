7dc0e17eba6f5054895d3c4a67adaaff329db875
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2023-05-25 08:31:02 +0500

    Fixed error

--- Schema/Nurseries/mutation.js
+++ Schema/Nurseries/mutation.js
@@ -6,7 +6,7 @@ export const NurseryMutation = {
   nurseryCreate: async (parent, args, ctx) => {
     const { data } = args;
     const user = ctx.user;
-    if (user.userType !== "NurseryOwner" && !args?.input?.nurseryOwnerID) {
+    if (user.userType !== "NurseryOwner" && !args?.data?.nurseryOwnerID) {
       throw new Error("Nursery Owner ID is required");
     }
     if (user.userType === "NurseryOwner") {
